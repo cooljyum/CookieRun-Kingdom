966a5277102c6627d36e2c26c69506a61310f450
Martin88Ko <kogitae@naver.com>
2024-06-07 14:40:06 +0900

    덱버튼 수정
    1. 덱 버튼 온클릭 이벤트 추가: 회색변환, 체크 이미지 활성화

--- CookieRunKingdom/Assets/2.Scripts/Ready/DeckSettingBtn.cs
+++ CookieRunKingdom/Assets/2.Scripts/Ready/DeckSettingBtn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,14 +11,17 @@ public class DeckSettingBtn : MonoBehaviour
 
     private Image _profileImage;
     private Image _typeImage;
+    private Image _check;
     private TextMeshProUGUI _levelData;
 
+    private bool _isSet = false;
 
     private void Awake()
     {
 
         _levelData = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
         _typeImage = transform.GetChild(2).GetComponent<Image>();
+        _check = transform.GetChild(3).GetComponent<Image>();
         _profileImage = GetComponent<Image>();
         
     }
@@ -28,4 +32,16 @@ public class DeckSettingBtn : MonoBehaviour
         _levelData.text = characterData.Level.ToString();
         _typeImage.sprite = characterData.typeImage;
     }
+
+    public void OnClick()
+    {
+        _isSet = !_isSet;
+
+        if(_isSet )
+        {
+            _check.gameObject.SetActive(true);
+            _profileImage.sprite.GetComponent<Image>().color = Color.gray;
+        }
+    }
+
 }
