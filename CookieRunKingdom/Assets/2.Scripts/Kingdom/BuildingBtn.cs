88a5640e3f20eaa2a3611dfecf9febae5aeca57c
Kim, Hyunbok <asd2590@naver.com>
2024-06-07 15:43:12 +0900

    건설 미리보기 및 레디씬 수정
    1. 건설 미리보기 스켈레톤 데이터 에셋 바꾸는거 수정
    2. 미리보기 마우스 따라다니기 수정
    3. 레디씬 캐릭터 베치구조 수정 및 정렬 스크립트 추가

--- CookieRunKingdom/Assets/2.Scripts/Kingdom/BuildingBtn.cs
+++ CookieRunKingdom/Assets/2.Scripts/Kingdom/BuildingBtn.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class BuildingBtn : MonoBehaviour
 {
-    [SerializeField]
     public BuildingData BuildingData;
+
+    private GameObject _activeImage;
+
+    private void Awake()
+    {
+        _activeImage = transform.parent.GetChild(4).gameObject;
+    }
 
     public void OnClickBuildingBtn()
     {
         print("Btn Click");
         KingdomManager.Instance.SelectBuilding(this);
+    }
+
+    public void SetActive(bool isActive)
+    {
+        _activeImage.SetActive(isActive);
     }
 }
--- CookieRunKingdom/Assets/2.Scripts/Manager/KingdomManager.cs
+++ CookieRunKingdom/Assets/2.Scripts/Manager/KingdomManager.cs
@@ -16,9 +16,11 @@ public class KingdomManager : MonoBehaviour
     [SerializeField]
     private List<GameObject> _myBuildingList = new List<GameObject>(); //내 빌딩 리스트
 
-    private GameObject _selectedBuilding;
-    private BuildingBtn _selectedBuildingBtn;
-    private SkeletonAnimation _selectedBuildingSA;
+    //private GameObject _selectedBuilding;
+    [SerializeField]
+    private SkeletonAnimation _selectedBuilding;
+
+    private BuildingBtn _selectedBuildingBtn;    
     private Transform _buildingBtnContent;
     private TextMeshProUGUI _buildingCost;
     private TextMeshProUGUI _buildingPoint;
@@ -42,33 +44,29 @@ public class KingdomManager : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        _selectedBuilding = GameObject.Find("SelectedBuilding");
-        _selectedBuilding.SetActive(false);
-        _selectedBuildingSA = _selectedBuilding.GetComponent<SkeletonAnimation>();
-
-    }
-
     private void OnEnable()
     {
-        StartCoroutine(BuildingControl());
+        //StartCoroutine(BuildingControl());
     }
 
     private void Update()
     {
+        if (!_selectedBuilding.gameObject.activeSelf) return;
 
+        Vector2 mousePos = Input.mousePosition;
+        _selectedBuilding.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(mousePos);
     }
 
     public void SelectBuilding(BuildingBtn buildingBtn)
     {
         _storePanel.SetActive(false);
         _selectedBuildingBtn = buildingBtn;
-        
-        string file = "SkeletonData/" + buildingBtn.BuildingData.Name + "_SkeletonData";
-        SkeletonAnimation skas = Resources.Load<SkeletonAnimation>(file);
-        _selectedBuildingSA = skas;
-        _selectedBuilding.SetActive(true);
+
+        //string file = "SkeletonData/" + buildingBtn.BuildingData.Name + "_SkeletonData";
+        //SkeletonAnimation skas = Resources.Load<SkeletonAnimation>(file);
+        _selectedBuilding.gameObject.SetActive(true);
+        _selectedBuilding.skeletonDataAsset = buildingBtn.BuildingData.SkeletonDataAsset;
+        _selectedBuilding.Initialize(true);
     }
     
     public int Building()
@@ -77,10 +75,10 @@ public class KingdomManager : MonoBehaviour
             return 0;
     
         int key = _selectedBuildingBtn.BuildingData.Key;
-    
-        _selectedBuildingBtn.transform.parent.GetChild(4).gameObject.SetActive(true);  //이렇게 접근하는 게 맞나
+
+        _selectedBuildingBtn.SetActive(true);
         _selectedBuildingBtn = null;
-        _selectedBuilding.SetActive(false);
+        _selectedBuilding.gameObject.SetActive(false);
     
         return key;
     }
@@ -91,14 +89,14 @@ public class KingdomManager : MonoBehaviour
         _storePanel.SetActive(true);
     }
 
-    private IEnumerator BuildingControl()
-    {
-        while (_selectedBuilding != null) 
-        {
-            //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            _selectedBuilding.transform.position = Input.mousePosition;
-
-            yield return null;
-        }
-    }
+    //private IEnumerator BuildingControl()
+    //{
+    //    while (_selectedBuilding != null) 
+    //    {
+    //        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        _selectedBuilding.transform.position = Input.mousePosition;
+    //
+    //        yield return null;
+    //    }
+    //}
 }
